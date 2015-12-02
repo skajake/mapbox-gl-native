552ef9607c00ea6e51326ae7319ad44d3aa1e66b
Minh Nguyễn <mxn@1ec5.org>
2015-12-02 00:51:56 -0800

    [osx] Removed unused NSBundle category code

--- platform/osx/NSBundle+MGLAdditions.m
+++ platform/osx/NSBundle+MGLAdditions.m
@@ -6,16 +6,8 @@ void mgl_linkBundleCategory() {}
 
 @implementation NSBundle (MGLAdditions)
 
-+ (instancetype)mgl_resourceBundle {
-    return [self bundleWithPath:self.mgl_resourceBundlePath];
-}
-
-+ (NSString *)mgl_resourceBundlePath {
-    NSString *resourceBundlePath = [self bundleForClass:[MGLAccountManager class]].resourcePath;
-    if (!resourceBundlePath) {
-        resourceBundlePath = self.mainBundle.resourcePath;
-    }
-    return resourceBundlePath;
++ (instancetype)mgl_frameworkBundle {
+    return [self bundleForClass:[MGLAccountManager class]];
 }
 
 @end
